14103cefe267d3c02a78685534a6eed4172ad6e6
RomaBortnik <bortnik25051998@gmail.com>
2023-04-28 14:17:27 +0300

    changes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import SubmitButton from './Button.styled';
 
 const Button = ({ onClick }) => {
   return (
-    <SubmitButton type="button" onClick={onClick} aria-label="submit">
+    <SubmitButton type="button" onClick={onClick}>
       Load more
     </SubmitButton>
   );
